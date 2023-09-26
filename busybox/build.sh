d2c603b6e3eb0899c953ad37ca9777acbe238e03
ryan <rsepassi@gmail.com>
2023-09-26 16:39:55 -0700

    Prefer to rewrite flags instead of patching

--- busybox/build.sh
+++ busybox/build.sh
@@ -5,14 +5,12 @@ ROOT=$PWD
 export PATH=$PATH:$PWD/clang-cross
 
 cd sources/busybox/busybox-1.36.0
-test -f ".patch" || git apply --directory=sources/busybox/busybox-1.36.0 $ROOT/patches/busybox.patch
-touch .patch
 make defconfig
 
 CFLAGS="-Wno-string-plus-int -Wno-ignored-optimization-argument -Wno-unused-command-line-argument -Wno-unused-result" \
 LDFLAGS="--static" \
 CROSS_COMPILE="clang-cross-" \
-make busybox_unstripped
+make -j128 busybox_unstripped
 
 zig objcopy --strip-all busybox_unstripped busybox
 chmod +x busybox
--- kernel/build.sh
+++ kernel/build.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 set -e
 
+export PATH=$PATH:$PWD/clang-cross
 cd sources/linux/linux-6.5
 
 # https://docs.kernel.org/kbuild/llvm.html
 clangmake() {
-  make LLVM=-16 ARCH=arm64 $1
+  make \
+    LLVM=1 \
+    ARCH=arm64 \
+    CC=clang-16 \
+    LD=ld.lld-16 \
+    AR=clang-cross-ar \
+    NM=llvm-nm-16 \
+    STRIP=llvm-strip-16 \
+    OBJCOPY=llvm-objcopy-16 \
+    OBJDUMP=llvm-objdump-16 \
+    READELF=llvm-readelf-16 \
+    HOSTCC=clang-cross-hostcc \
+    HOSTCXX=clang-c++-16 \
+    HOSTAR=clang-cross-ar \
+    HOSTLD=ld.lld-16 \
+    $1
 }
 
 clangmake defconfig
