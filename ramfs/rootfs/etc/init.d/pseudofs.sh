0fb334121d9432cb1eb221a5962f38e9a6e6d483
Ryan Sepassi <rsepassi@gmail.com>
2024-07-29 23:46:56 -0700

    add mqueue and kcore symlink

--- ramfs/rootfs/etc/init.d/pseudofs.sh
+++ ramfs/rootfs/etc/init.d/pseudofs.sh
@@ -7,12 +7,14 @@ mount -t tmpfs    tmpfs    /tmp
 
 # /dev
 mount -t devtmpfs devtmpfs /dev
-mkdir /dev/shm /dev/pts
+mkdir /dev/shm /dev/pts /dev/mqueue
 mount -t tmpfs     tmpfs     /dev/shm
 mount -t devpts    devpts    /dev/pts
+mount -t mqueue    mqueue    /dev/mqueue
 
 ln -s /dev/rtc0 /dev/rtc
 ln -s /proc/self/fd /dev/fd
+ln -s /proc/kcore /dev/core
 ln -s /proc/self/fd/0 /dev/stdin
 ln -s /proc/self/fd/1 /dev/stdout
 ln -s /proc/self/fd/2 /dev/stderr
